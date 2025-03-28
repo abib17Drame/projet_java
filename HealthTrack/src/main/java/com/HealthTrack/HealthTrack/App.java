beebf5b92bb1d46a6b4fd0a318d50fd5d9980f59
Mouhamadou Abib DRAME <drameabib70@gmail.com>
2025-03-28 21:42:59 +0000

    Ajout fichiers pour la connexion a la base de donnee

---
+++ HealthTrack/src/main/java/com/HealthTrack/HealthTrack/App.java
@@ -0,0 +1,10 @@
+package com.HealthTrack.HealthTrack;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
